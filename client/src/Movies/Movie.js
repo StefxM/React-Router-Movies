0b80d177d40f6c5c2f95256f48b6fdd48697c993
stephanie mairena <stefyem7711@gmail.com>
2020-03-15 23:01:22 -0400

    mvp

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -5,7 +5,7 @@ const Movie = (props) => {
   const [movie, setMovie] = useState();
  
   useEffect(() => {
-    const id = 1;
+    const id = props.match.params.id;
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
 
@@ -20,6 +20,8 @@ const Movie = (props) => {
 
   },[]);
   
+  console.log(props);
+
   // Uncomment this only when you have moved on to the stretch goals
   // const saveMovie = () => {
   //   const addToSavedList = props.addToSavedList;
